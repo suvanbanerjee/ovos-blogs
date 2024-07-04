3e16e29e4c89fa6e77efa2c34b08a78a5f6bc12d
suvanbanerjee <banerjeesuvan@gmail.com>
2024-07-04 17:37:41 +0530

    header ?

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,16 +2,16 @@ import Link from 'next/link';
 import Logo from "./logo";
 
 export default function Header() {
-  return (
-      <header>
-          <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
-              <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                  <Logo />
-                  <div className="flex items-center">
-                      <Link href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Home</Link>
-                  </div>
-              </div>
-          </nav>
-      </header>
-  );
+    return (
+        <header className='sticky top-0 z-50 backdrop-blur-md'>
+            <nav className="bg-white/90 dark:bg-stone-900/90 border-gray-200 px-4 mt-2 lg:px-6 py-2.5">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <Logo />
+                    <div className="flex items-center">
+                        <Link href="#" className="text-white dark:text-black bg-black dark:bg-white font-bold rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2">Home</Link>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
 }
