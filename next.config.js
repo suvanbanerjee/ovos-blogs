e2f9d13af574141ed34dcbb75894cfc3a7ed0c2d
suvanbanerjee <banerjeesuvan@gmail.com>
2024-07-04 18:50:02 +0530

    fixing deployment

--- next.config.js
+++ next.config.js
@@ -5,7 +5,12 @@ const {withContentlayer} = require("next-contentlayer")
 const nextConfig = {
     compiler:{
         removeConsole: true,
-    }
+    },
+    images: {
+        unoptimized: true,
+    },
+    output: "export",
+    basePath: "/nextjs-github-pages"
 };
 
 module.exports = withContentlayer({ ...nextConfig });
